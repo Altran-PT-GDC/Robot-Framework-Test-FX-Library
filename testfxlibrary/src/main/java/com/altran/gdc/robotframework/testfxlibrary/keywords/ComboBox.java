192d02f764a01b0b8874b55fa858dfd3006a4f79
Lourenço Sérgio <sergio.lourenco@altran.com>
2017-08-30 15:34:51 +0100

    new keywords for ComboBox and ListView

--- testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/ComboBox.java
+++ testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/ComboBox.java
@@ -12,6 +12,16 @@ import java.util.List;
 @RobotKeywords
 public class ComboBox {
 
+
+    /**
+     * Get values from ComboBox.
+     *
+     * @param identifier
+     *      The id of the combobox.
+     *
+     * @return
+     *      The list of values
+     */
     @RobotKeyword
     @ArgumentNames({"identifier"})
     public List<String> getListItemsFromComboBox(String identifier){
@@ -20,18 +30,57 @@ public class ComboBox {
         javafx.scene.control.ComboBox comboBox = new FxRobot().lookup(identifier).query();
 
         List<String> list = new ArrayList<>();
-        comboBox.getItems().forEach((item) -> {
-            list.add((String) item);
-        });
+        comboBox.getItems().forEach(item ->
+            list.add((String) item)
+        );
 
         return list;
     }
 
-    public void selectFromComboBox(String identifier, String text, int position){
+    /**
+     * Select item from ComboBox by text.
+     * @param identifier
+     *      The id of the ComboBox.
+     *
+     * @param text
+     *      The text to get the selected item
+     */
+    @RobotKeyword
+    @ArgumentNames({"identifier", "text"})
+    public void selectFromComboBoxByText(String identifier, String text){
         TestFxLibraryValidation.validateArguments(identifier, text);
-        TestFxLibraryValidation.validateIndex(position);
 
+        javafx.scene.control.ComboBox comboBox = new FxRobot().lookup(identifier).query();
 
+        new FxRobot().clickOn(comboBox);
+
+        comboBox.getItems().forEach(item -> {
+            if(((String)item).equals(text)){
+                comboBox.getSelectionModel().select(item);
+            }
+        });
     }
 
+    /**
+     * Select item from ComboBox by position.
+     *
+     * @param identifier
+     *      The id of the ComboBox
+     *
+     * @param position
+     *      The position to get the selected item
+     */
+    @RobotKeyword
+    @ArgumentNames({"identifier", "text"})
+    public void selectFromComboBoxByPosition(String identifier, int position){
+        TestFxLibraryValidation.validateArguments(identifier);
+        TestFxLibraryValidation.validateIndex(position);
+
+        javafx.scene.control.ComboBox comboBox = new FxRobot().lookup(identifier).query();
+
+        new FxRobot().clickOn(comboBox);
+
+        comboBox.getSelectionModel().select(position);
+
+    }
 }
--- testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/List.java
+++ testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/List.java
@@ -12,6 +12,15 @@ import java.util.ArrayList;
 @RobotKeywords
 public class List {
 
+    /**
+     * Get values from ListView.
+     *
+     * @param identifier
+     *      The id of the ListView
+     *
+     * @return
+     *      The list of values
+     */
     @RobotKeyword
     @ArgumentNames({"identifier"})
     public java.util.List<String> getListItemsFromListView(String identifier){
@@ -25,6 +34,54 @@ public class List {
         });
 
         return list;
+    }
+
+    /**
+     * Select item from ListView by text.
+     *
+     * @param identifier
+     *      The id of the ListView.
+     *
+     * @param text
+     *      The text to get the selected item
+     */
+    @RobotKeyword
+    @ArgumentNames({"identifier", "text"})
+    public void selectFromListViewByText(String identifier, String text){
+        TestFxLibraryValidation.validateArguments(identifier, text);
+
+        ListView listView = new FxRobot().lookup(identifier).query();
+
+        new FxRobot().clickOn(listView);
+
+        listView.getItems().forEach((item) -> {
+            if(((String)item).equals(text)){
+                listView.getSelectionModel().select(item);
+            }
+        });
+    }
+
+    /**
+     * Select item from ListView by position.
+     *
+     * @param identifier
+     *      The id of the ListView
+     *
+     * @param position
+     *      The position to get the selected item
+     */
+    @RobotKeyword
+    @ArgumentNames({"identifier", "text"})
+    public void selectFromListViewByPosition(String identifier, int position){
+        TestFxLibraryValidation.validateArguments(identifier);
+        TestFxLibraryValidation.validateIndex(position);
+
+        ListView listView = new FxRobot().lookup(identifier).query();
+
+        new FxRobot().clickOn(listView);
+
+        listView.getSelectionModel().select(position);
+
     }
 
 }
