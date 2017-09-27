b728019101ddf7c8e370683a2ab11d2e2f64cdbd
Joao Gomes <joaodaniel.gomes@altran.com>
2017-09-27 15:54:14 +0100

    Added Sonar corrections to JavaFxExample3 and added comments to the code

--- testfxlibrary/src/main/java/testapp/JavaFxExample3.java
+++ testfxlibrary/src/main/java/testapp/JavaFxExample3.java
@@ -35,32 +35,51 @@ public class JavaFxExample3 extends Application {
         }
     }
 
+    private static final String FIRST_BUTTON_TEXT = new String("Button 1");
+    private static final String SECOND_BUTTON_TEXT = new String("Button 2");
+    private static final String THIRD_BUTTON_TEXT = new String("Button 3");
+    private static final String FOURTH_BUTTON_TEXT = new String("Button 4");
+    private static  final int DEFAULT_SPACING = 10;
+    private static  final int TOP_PADDING = 10;
+    private static  final int RIGHT_PADDING = 10;
+    private static  final int BOTTOM_PADDING = 10;
+    private static  final int LEFT_PADDING = 10;
+    private static  final int MAIN_PANE_WIDTH = 800;
+    private static  final int MAIN_PANE_HEIGHT = 600;
+    private static  final int SEC_PANE_WIDTH = 50;
+    private static  final int SEC_PANE_HEIGHT = 50;
+    private static  final int TABLE_WIDTH = 370;
+    private static  final int TABLE_HEIGHT = 170;
+    private static  final int LIST_VIEW_MAX_HEIGHT = 200;
+    private static  final int DEFAULT_FONT_SIZE = 12;
+    private static  final int LIST_ITEMS = 10;
+
     public static void main(String[] args) {
         launch(args);
     }
 
+
     @Override
     public void start(Stage primaryStage) throws InterruptedException {
+        //Set main pane
         Pane p = new Pane();
-        p.setPrefSize(800, 600);
+        p.setPrefSize(MAIN_PANE_WIDTH, MAIN_PANE_HEIGHT);
 
+        //Set master VBox and HBox
         VBox vBoxMaster = new VBox();
         HBox hBoxMaster1 = new HBox();
         HBox hBoxMaster2 = new HBox();
         p.getChildren().addAll(vBoxMaster);
         vBoxMaster.getChildren().addAll(hBoxMaster1, hBoxMaster2);
-        vBoxMaster.setPadding(new Insets(10,10,10,10));
-        hBoxMaster1.setPadding(new Insets(10,10,10,10));
-        hBoxMaster2.setPadding(new Insets(10,10,10,10));
         vBoxMaster.autosize();
 
         BorderPane border = new BorderPane();
-        border.setPadding(new Insets(20, 0, 20, 20));
 
-        Button btn1 = new Button("Button 1");
-        Button btn2 = new Button("Button 2");
-        Button btn3 = new Button("Button 3");
-        Button btn4 = new Button("Button 4");
+        //Buttons with same text
+        Button btn1 = new Button(FIRST_BUTTON_TEXT);
+        Button btn2 = new Button(SECOND_BUTTON_TEXT);
+        Button btn3 = new Button(THIRD_BUTTON_TEXT);
+        Button btn4 = new Button(FOURTH_BUTTON_TEXT);
 
         btn1.setMaxWidth(Double.MAX_VALUE);
         btn2.setMaxWidth(Double.MAX_VALUE);
@@ -68,13 +87,13 @@ public class JavaFxExample3 extends Application {
         btn4.setMaxWidth(Double.MAX_VALUE);
 
         VBox vbButtons1 = new VBox();
-        vbButtons1.setSpacing(10);
+        vbButtons1.setSpacing(DEFAULT_SPACING);
         vbButtons1.getChildren().addAll(btn1, btn2, btn3, btn4);
 
-        Button btn5 = new Button("Button 1");
-        Button btn6 = new Button("Button 2");
-        Button btn7 = new Button("Button 3");
-        Button btn8 = new Button("Button 4");
+        Button btn5 = new Button(FIRST_BUTTON_TEXT);
+        Button btn6 = new Button(SECOND_BUTTON_TEXT);
+        Button btn7 = new Button(THIRD_BUTTON_TEXT);
+        Button btn8 = new Button(FOURTH_BUTTON_TEXT);
 
         btn5.setMaxWidth(Double.MAX_VALUE);
         btn6.setMaxWidth(Double.MAX_VALUE);
@@ -82,28 +101,29 @@ public class JavaFxExample3 extends Application {
         btn8.setMaxWidth(Double.MAX_VALUE);
 
         VBox vbButtons2 = new VBox();
-        vbButtons2.setSpacing(10);
+        vbButtons2.setSpacing(DEFAULT_SPACING);
         vbButtons2.getChildren().addAll(btn5, btn6, btn7, btn8);
 
+        //Buttons with different parent IDs
         HBox hBox1 = new HBox();
         HBox hBoxButtons = new HBox();
         Label labelButtons = new Label("Different parent ID's:");
-        labelButtons.setFont(Font.font(null, FontWeight.BOLD, 12));
+        labelButtons.setFont(Font.font(null, FontWeight.BOLD, DEFAULT_FONT_SIZE));
         hBoxButtons.getChildren().addAll(labelButtons);
 
         hBox1.getChildren().addAll(vbButtons1, vbButtons2);
 
         VBox vBox1 = new VBox();
-        vBox1.setSpacing(10);
+        vBox1.setSpacing(DEFAULT_SPACING);
         vBox1.getChildren().addAll(hBoxButtons, hBox1);
 
         vbButtons1.setId("vbuttons1");
         vbButtons2.setId("vbuttons2");
 
-        Button btn9 = new Button("Button 1");
-        Button btn10 = new Button("Button 2");
-        Button btn11 = new Button("Button 3");
-        Button btn12 = new Button("Button 4");
+        Button btn9 = new Button(FIRST_BUTTON_TEXT);
+        Button btn10 = new Button(SECOND_BUTTON_TEXT);
+        Button btn11 = new Button(THIRD_BUTTON_TEXT);
+        Button btn12 = new Button(FOURTH_BUTTON_TEXT);
 
         btn9.setMaxWidth(Double.MAX_VALUE);
         btn10.setMaxWidth(Double.MAX_VALUE);
@@ -113,11 +133,11 @@ public class JavaFxExample3 extends Application {
         VBox vbButtons3 = new VBox();
         vbButtons3.setId("vbuttons1");
         Label labelButtons2 = new Label("Same parent ID 1st Column:");
-        labelButtons2.setFont(Font.font(null, FontWeight.BOLD, 12));
-        vbButtons3.setSpacing(10);
-        //vbButtons3.setPadding(new Insets(10, 20, 10, 220));
+        labelButtons2.setFont(Font.font(null, FontWeight.BOLD, DEFAULT_FONT_SIZE));
+        vbButtons3.setSpacing(DEFAULT_SPACING);
         vbButtons3.getChildren().addAll(labelButtons2, btn9, btn10, btn11, btn12);
 
+        //Combo boxes with same buttons
         ObservableList<String> observableList1 =
                 FXCollections.observableArrayList(
                         "Option 1",
@@ -143,15 +163,16 @@ public class JavaFxExample3 extends Application {
         vBoxCombo.setId("vboxcombo");
         Label labelCombo = new Label();
         labelCombo.setText("Comboboxes with same items and same parents:");
-        labelCombo.setFont(Font.font(null, FontWeight.BOLD, 12));
+        labelCombo.setFont(Font.font(null, FontWeight.BOLD, DEFAULT_FONT_SIZE));
 
+        //Open new windows with button
         Button buttonNewWindow = new Button("Open New Window");
         buttonNewWindow.setId("btnwindow");
         buttonNewWindow.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Pane p2 = new Pane();
-                p2.setPrefSize(50, 50);
+                p2.setPrefSize(SEC_PANE_WIDTH, SEC_PANE_HEIGHT);
                 Scene scene2 = new Scene(p2);
                 Stage secondaryStage = new Stage();
                 secondaryStage.setScene(scene2);
@@ -160,13 +181,14 @@ public class JavaFxExample3 extends Application {
             }
         });
 
+        //Open new window second button
         Button buttonNewWindow2 = new Button("Open New Window V2");
         buttonNewWindow2.setId("btnwindow2");
         buttonNewWindow2.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 Pane p3 = new Pane();
-                p3.setPrefSize(50, 50);
+                p3.setPrefSize(SEC_PANE_WIDTH, SEC_PANE_HEIGHT);
                 Scene scene3 = new Scene(p3);
                 Stage thirdStage = new Stage();
                 thirdStage.setScene(scene3);
@@ -176,30 +198,31 @@ public class JavaFxExample3 extends Application {
         });
 
         vBoxCombo.getChildren().addAll(hBoxCombo2, hBoxCombo1, buttonNewWindow, buttonNewWindow2);
-        vBoxCombo.setSpacing(10);
-        hBoxCombo1.setSpacing(10);
+        vBoxCombo.setSpacing(DEFAULT_SPACING);
+        hBoxCombo1.setSpacing(DEFAULT_SPACING);
         hBoxCombo1.getChildren().addAll(comboBox1, comboBox2);
         hBoxCombo2.getChildren().addAll(labelCombo);
 
+        //List view with text and buttons for each element
         VBox vBoxListView = new VBox();
-        ListView<HBoxCell> listView1 = new ListView<HBoxCell>();
+        ListView<HBoxCell> listView1 = new ListView<>();
         Label labelListView = new Label("Multiple Selection List View:");
-        labelListView.setFont(Font.font(null, FontWeight.BOLD, 12));
-        listView1.setMaxHeight(200);
+        labelListView.setFont(Font.font(null, FontWeight.BOLD, DEFAULT_FONT_SIZE));
+        listView1.setMaxHeight(LIST_VIEW_MAX_HEIGHT);
         listView1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         List<HBoxCell> list = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            list.add(new HBoxCell("Item " + i, "Add Interpretation"));
+        for (int i = 0; i < LIST_ITEMS; i++) {
+            list.add(new HBoxCell("Item " + i, "Hyperlink Button"));
         }
         ObservableList<HBoxCell> myObservableList = FXCollections.observableList(list);
         listView1.setItems(myObservableList);
 
+        //Tables with same items and same IDs
         HBox hBoxTables = new HBox();
         hBoxTables.setId("hboxtables");
         VBox vBoxTables = new VBox();
         hBoxTables.getChildren().add(vBoxTables);
 
-        TableRow tableRow1 = new TableRow();
         final ObservableList<Person> data =
                 FXCollections.observableArrayList(
                         new Person("Jacob", "Smith", "jacob.smith@example.com"),
@@ -216,8 +239,10 @@ public class JavaFxExample3 extends Application {
                         new Person("Emma", "Jones", "emma.jones@example.com"),
                         new Person("Michael", "Brown", "michael.brown@example.com")
                 );
-        TableView<Person> tableView1 = new TableView<Person>();
-        TableView<Person> tableView2 = new TableView<Person>();
+        TableView<Person> tableView1 = new TableView<>();
+        TableView<Person> tableView2 = new TableView<>();
+
+        //Table columns definition
         TableColumn firstNameCol = new TableColumn("First Name");
         firstNameCol.setCellValueFactory(new PropertyValueFactory<Person,String>("firstName"));
         TableColumn lastNameCol = new TableColumn("Last Name");
@@ -225,7 +250,6 @@ public class JavaFxExample3 extends Application {
                 new PropertyValueFactory<Person,String>("lastName")
         );
         TableColumn emailCol = new TableColumn("Email");
-        emailCol.setMinWidth(200);
         emailCol.setCellValueFactory(
                 new PropertyValueFactory<Person,String>("email")
         );
@@ -236,7 +260,6 @@ public class JavaFxExample3 extends Application {
                 new PropertyValueFactory<Person,String>("lastName")
         );
         TableColumn emailCol2 = new TableColumn("Email");
-        emailCol2.setMinWidth(200);
         emailCol2.setCellValueFactory(
                 new PropertyValueFactory<Person,String>("email")
         );
@@ -247,17 +270,23 @@ public class JavaFxExample3 extends Application {
         tableView1.getColumns().addAll(firstNameCol, lastNameCol, emailCol);
         tableView2.setItems(data2);
         tableView2.getColumns().addAll(firstNameCol2, lastNameCol2, emailCol2);
-        tableView1.setPrefSize(370, 170);
-        tableView2.setPrefSize(370, 170);
+        tableView1.setPrefSize(TABLE_WIDTH, TABLE_HEIGHT);
+        tableView2.setPrefSize(TABLE_WIDTH, TABLE_HEIGHT);
         vBoxTables.getChildren().addAll(tableView1, tableView2);
         vBoxListView.getChildren().addAll(labelListView, listView1);
 
-        vBox1.setPadding(new Insets(10,10,10,10));
-        vbButtons3.setPadding(new Insets(10,10,10,10));
-        vBoxCombo.setPadding(new Insets(10,10,10,10));
-        vBoxListView.setPadding(new Insets(10,10,10,10));
-        hBoxTables.setPadding(new Insets(10,10,10,10));
+        //Elements padding
+        vBoxMaster.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        hBoxMaster1.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        hBoxMaster2.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        border.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        vBox1.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        vbButtons3.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        vBoxCombo.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        vBoxListView.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
+        hBoxTables.setPadding(new Insets(TOP_PADDING,RIGHT_PADDING,BOTTOM_PADDING,LEFT_PADDING));
 
+        //Add elements to master elements and start primary stage
         hBoxMaster1.getChildren().addAll(vBox1, vbButtons3, vBoxCombo);
         hBoxMaster2.getChildren().addAll(vBoxListView, hBoxTables);
         Scene scene = new Scene(p);
