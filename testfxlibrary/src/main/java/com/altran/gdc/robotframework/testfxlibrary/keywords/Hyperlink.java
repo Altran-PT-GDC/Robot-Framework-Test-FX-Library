c95aed055eb5fe84c55d3b5b5f051671b6019c26
diribeiro <diogo.ribeiro@altran.com>
2017-10-09 14:02:04 +0100

    Added Hyperlink Keywords and corrected some sonar issues

--- testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/Hyperlink.java
+++ testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/Hyperlink.java
@@ -19,7 +19,7 @@ public class Hyperlink {
      *  <i>identifier</i>.
      *
      * @param identifier
-     * : the id of the component with a tooltip
+     * : the id of the Hyperlink component
      * <br><br>
      * <table summary="">
      *     <tr>
@@ -59,6 +59,213 @@ public class Hyperlink {
 
         try {
             return hyperText.getText();
+        } catch (IllegalArgumentException | NullPointerException e){
+            throw  new TestFxLibraryFatalException(e);
+        }
+    }
+
+    /**
+     * <b>Description:</b> This keyword returns the status of an Hyperlink specified by an
+     *  <i>identifier</i>.
+     *
+     * @param identifier
+     * : the id of the Hyperlink component
+     * <br><br>
+     * <table summary="">
+     *     <tr>
+     *         <th>Parameter</th>
+     *         <th>Mandatory</th>
+     *         <th>Values</th>
+     *         <th>Default</th>
+     *     </tr>
+     *     <tr>
+     *         <td>identifier</td>
+     *         <td>Yes</td>
+     *         <td>string</td>
+     *         <td>N/A</td>
+     *     </tr>
+     * </table>
+     *
+     * @return
+     *  The current status of an Hyperlink
+     *
+     * <br><br>
+     * <b>Examples:</b>
+     * <table summary="">
+     *     <tr>
+     *         <td>/#hyperlink</td>
+     *         <td>Get Hyperlink Status</td>
+     *     </tr>
+     * </table>
+     */
+
+    @RobotKeyword
+    @ArgumentNames({"identifier"})
+    public Boolean getHyperlinkStatus(String identifier){
+        TestFxLibraryValidation.validateArguments(identifier);
+        wait.waitUntilPageContains(identifier);
+
+        javafx.scene.control.Hyperlink hyperlink = TestFxLibraryCommon.lookup(identifier);
+
+        try {
+            return hyperlink.isVisited();
+        } catch (IllegalArgumentException | NullPointerException e){
+            throw  new TestFxLibraryFatalException(e);
+        }
+    }
+
+    /**
+     * <b>Description:</b> This keyword set the status of an Hyperlink specified by an
+     *  <i>identifier</i>.
+     *
+     * @param identifier
+     * : the id of the Hyperlink component
+     * @param value
+     * : the value of the Hyperlink status
+     * <br><br>
+     * <table summary="">
+     *     <tr>
+     *         <th>Parameter</th>
+     *         <th>Mandatory</th>
+     *         <th>Values</th>
+     *         <th>Default</th>
+     *     </tr>
+     *     <tr>
+     *         <td>identifier</td>
+     *         <td>Yes</td>
+     *         <td>string</td>
+     *         <td>N/A</td>
+     *     </tr>
+     *      <tr>
+     *         <td>value</td>
+     *         <td>Yes</td>
+     *         <td>string</td>
+     *         <td>N/A</td>
+     *     </tr>
+     * </table>
+     * <br><br>
+     * <b>Examples:</b>
+     * <table summary="">
+     *     <tr>
+     *         <td>/#hyperlink</td>
+     *         <td>Set Hyperlink Status</td>
+     *         <td>true</td>
+     *     </tr>
+     * </table>
+     */
+
+    @RobotKeyword
+    @ArgumentNames({"identifier", "value"})
+    public void setHyperlinkStatus(String identifier, String value){
+        TestFxLibraryValidation.validateArguments(identifier);
+        wait.waitUntilPageContains(identifier);
+
+        javafx.scene.control.Hyperlink hyperlink = TestFxLibraryCommon.lookup(identifier);
+
+        try {
+            hyperlink.setVisited(Boolean.parseBoolean(value));
+        } catch (IllegalArgumentException | NullPointerException e){
+            throw  new TestFxLibraryFatalException(e);
+        }
+    }
+
+    /**
+     * <b>Description:</b> This keyword check if Hyperlink status specified by an
+     *  <i>identifier</i> is visited.
+     *
+     * @param identifier
+     * : the id of the Hyperlink component
+     * <br><br>
+     * <table summary="">
+     *     <tr>
+     *         <th>Parameter</th>
+     *         <th>Mandatory</th>
+     *         <th>Values</th>
+     *         <th>Default</th>
+     *     </tr>
+     *     <tr>
+     *         <td>identifier</td>
+     *         <td>Yes</td>
+     *         <td>string</td>
+     *         <td>N/A</td>
+     *     </tr>
+     * </table>
+     * <br><br>
+     * <b>Examples:</b>
+     * <table summary="">
+     *     <tr>
+     *         <td>/#hyperlink</td>
+     *         <td>Hyperlink Should Be Visited</td>
+     *     </tr>
+     * </table>
+     */
+
+    @RobotKeyword
+    @ArgumentNames({"identifier"})
+    public void hyperlinkShouldBeVisited(String identifier){
+        TestFxLibraryValidation.validateArguments(identifier);
+        wait.waitUntilPageContains(identifier);
+
+        javafx.scene.control.Hyperlink hyperlink = TestFxLibraryCommon.lookup(identifier);
+
+
+        try {
+            if(!hyperlink.isVisited()){
+
+                throw new TestFxLibraryFatalException("Hyperlink was not visited yet.");
+
+            }
+        } catch (IllegalArgumentException | NullPointerException e){
+            throw  new TestFxLibraryFatalException(e);
+        }
+    }
+
+    /**
+     * <b>Description:</b> This keyword check if Hyperlink status specified by an
+     *  <i>identifier</i> is visited.
+     *
+     * @param identifier
+     * : the id of the Hyperlink component
+     * <br><br>
+     * <table summary="">
+     *     <tr>
+     *         <th>Parameter</th>
+     *         <th>Mandatory</th>
+     *         <th>Values</th>
+     *         <th>Default</th>
+     *     </tr>
+     *     <tr>
+     *         <td>identifier</td>
+     *         <td>Yes</td>
+     *         <td>string</td>
+     *         <td>N/A</td>
+     *     </tr>
+     * </table>
+     * <br><br>
+     * <b>Examples:</b>
+     * <table summary="">
+     *     <tr>
+     *         <td>/#hyperlink</td>
+     *         <td>Hyperlink Should Not Be Visited</td>
+     *     </tr>
+     * </table>
+     */
+
+    @RobotKeyword
+    @ArgumentNames({"identifier"})
+    public void hyperlinkShouldNotBeVisited(String identifier){
+        TestFxLibraryValidation.validateArguments(identifier);
+        wait.waitUntilPageContains(identifier);
+
+        javafx.scene.control.Hyperlink hyperlink = TestFxLibraryCommon.lookup(identifier);
+
+
+        try {
+            if(hyperlink.isVisited()){
+
+                throw new TestFxLibraryFatalException("Hyperlink was visited already.");
+
+            }
         } catch (IllegalArgumentException | NullPointerException e){
             throw  new TestFxLibraryFatalException(e);
         }
--- testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/RadioButton.java
+++ testfxlibrary/src/main/java/com/altran/gdc/robotframework/testfxlibrary/keywords/RadioButton.java
@@ -3,15 +3,10 @@ package com.altran.gdc.robotframework.testfxlibrary.keywords;
 import com.altran.gdc.robotframework.testfxlibrary.exceptions.TestFxLibraryNonFatalException;
 import com.altran.gdc.robotframework.testfxlibrary.utils.TestFxLibraryCommon;
 import com.altran.gdc.robotframework.testfxlibrary.utils.TestFxLibraryValidation;
-import javafx.scene.Node;
-import javafx.scene.control.Control;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ToggleGroup;
 import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.Autowired;
 import org.robotframework.javalib.annotation.RobotKeyword;
 import org.robotframework.javalib.annotation.RobotKeywords;
-import org.testfx.api.FxRobot;
 
 
 @RobotKeywords
@@ -25,14 +20,15 @@ public class RadioButton {
 
     @RobotKeyword
     @ArgumentNames({"identifier"})
-    public void radioButtonShouldBeSelected(String identifier) throws TestFxLibraryNonFatalException {
+    public void radioButtonShouldBeSelected(String identifier) {
         TestFxLibraryValidation.validateArguments(identifier);
         Boolean flag=false;
 
         try {
             javafx.scene.control.RadioButton radiobutton = TestFxLibraryCommon.lookup(identifier);
-            if(radiobutton.isSelected())
-                flag=true;
+            if(radiobutton.isSelected()) {
+                flag = true;
+            }
 
         }catch (Exception e){
             throw new TestFxLibraryNonFatalException(String.format(IDENTIFIER_NOT_EXIST, identifier), e);
